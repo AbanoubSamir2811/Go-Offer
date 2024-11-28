1df3f5d8406ab2012b5de088d6c513a59026e1b7
AbanoubSamir2811 <abanoubsamir2811@gmail.com>
2024-11-29 01:39:58 +0200

    update

--- src/app/shared/components/BrandCard.js
+++ src/app/shared/components/BrandCard.js
@@ -6,7 +6,7 @@ function BrandCard({store}) {
   return (
     <div className="flex flex-col text-center items-center justify-evenly ms-0 min-w-[293px] h-[169px] bg-white">
         {/* Store Image */}
-        <Image
+        <img
             className="rounded-[15px] rounded-y-[10px] w-auto h-[61px]"
             src={store.logo}
             alt={store.name}
